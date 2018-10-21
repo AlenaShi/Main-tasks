8e21b25764aa536db0b056cbb43ed87c5efc7199
AlenaShi <helen.shir.helen@gmail.com>
2018-10-21 21:44:27 +0300

    ch

--- PracticalTask02/src/first/Task02.java
+++ PracticalTask02/src/first/Task02.java
@@ -1,19 +1,19 @@
 package first;
 
 public class Task02 {
-	public static double getA(double x1, double y1, double x2, double y2) {
+	static double getA(double x1, double y1, double x2, double y2) {
 		return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 	}
 
-	public static double getB(double x2, double y2, double x3, double y3) {
+	static double getB(double x2, double y2, double x3, double y3) {
 		return Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2));
 	}
 
-	public static double getC(double x1, double y1, double x3, double y3) {
+	static double getC(double x1, double y1, double x3, double y3) {
 		return Math.sqrt(Math.pow((x1 - x3), 2) + Math.pow((y1 - y3), 2));
 	}
 
-	public static boolean isTriangular(double x1, double y1, double x2, double y2, double x3, double y3) {
+	static boolean isTriangular(double x1, double y1, double x2, double y2, double x3, double y3) {
 		double a = Task02.getA(x1, y1, x2, y2);
 		double b = Task02.getB(x2, y2, x3, y3);
 		double c = Task02.getC(x1, y1, x3, y3);
@@ -21,7 +21,7 @@ public class Task02 {
 		return a + b > c && a + c > b && b + c > a;
 	}
 
-	public static boolean isReactTriangular(double x1, double y1, double x2, double y2, double x3, double y3) {
+	static boolean isReactTriangular(double x1, double y1, double x2, double y2, double x3, double y3) {
 		double a = Task02.getA(x1, y1, x2, y2);
 		double b = Task02.getB(x2, y2, x3, y3);
 		double c = Task02.getC(x1, y1, x3, y3);
