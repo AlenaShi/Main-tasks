ec677f65e91598c0c040e80fbb8fda2dafceb5a7
AlenaShi <helen.shir.helen@gmail.com>
2018-10-28 14:44:07 +0300

    +log4j

--- PracticalTask03/src/com/epam/javatraining/alenashi/tasks/Test01.java
+++ PracticalTask03/src/com/epam/javatraining/alenashi/tasks/Test01.java
@@ -1,11 +1,15 @@
 package com.epam.javatraining.alenashi.tasks;
 
+import org.apache.log4j.Logger;
+
 public class Test01 {
+public static final Logger LOG = Logger.getRootLogger();
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		String result = Task01.getCount();
-		View.print(result);
+		LOG.info(result);
 
 	}
 
