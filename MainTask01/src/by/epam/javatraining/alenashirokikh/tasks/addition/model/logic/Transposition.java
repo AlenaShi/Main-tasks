985cf330d756c9450cd7d96ab5a9c1a956cd4f09
AlenaShi <helen.shir.helen@gmail.com>
2018-11-01 21:03:40 +0300

    addition

---
+++ MainTask01/src/by/epam/javatraining/alenashirokikh/tasks/addition/model/logic/Transposition.java
@@ -0,0 +1,26 @@
+package by.epam.javatraining.alenashirokikh.tasks.addition.model.logic;
+
+public class Transposition {
+	public static double[][] transposeMatrix(double[][] matrix) {
+		int k = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0+k; j < matrix[0].length-k-1; j++) {
+				if(matrix[matrix.length - j - 1][i]== matrix[i][j]){
+					continue;
+				}else{
+				matrix[matrix.length - j - 1][i] = matrix[matrix.length - j - 1][i] + matrix[i][j]
+						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i];
+				matrix[matrix.length - 1 - i][matrix.length - j - 1] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
+						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+				matrix[j][matrix.length - 1 - i] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
+						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+				matrix[i][j] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
+						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+				matrix[matrix.length - j - 1][i] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
+						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+			}}
+			++k;
+		}
+		return matrix;
+	}
+}
