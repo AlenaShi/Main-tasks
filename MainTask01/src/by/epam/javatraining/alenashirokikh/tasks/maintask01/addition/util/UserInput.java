d299628678438fa1551746e5fd579058ef19d4cd
AlenaShi <helen.shir.helen@gmail.com>
2018-11-01 19:54:12 +0300

    addition

---
+++ MainTask01/src/by/epam/javatraining/alenashirokikh/tasks/maintask01/addition/util/UserInput.java
@@ -0,0 +1,37 @@
+package by.epam.javatraining.alenashirokikh.tasks.maintask01.addition.util;
+
+/**
+ * The program helps input number or the array with a given size.
+ * 
+ * @author Alena Shirokikh
+ * @version 1.0 1.11.2018
+ */
+import java.util.Scanner;
+
+public class UserInput {
+	static Scanner in;
+	static {
+		in = new Scanner(System.in);
+	}
+
+	public static int readNumber(String msg) {
+		System.out.println(msg);
+		return in.nextInt();
+	}
+
+	public static double[][] readMatrixWithSize(String msg) {
+		int colMatrix = UserInput.readNumber("Enter number of columns");
+		int strMatrix = UserInput.readNumber("Enter number of strings");
+		System.out.println(msg);
+		double[][] matrix = new double[colMatrix][strMatrix];
+
+		for (int i = 0; i < colMatrix; i++) {
+			for (int j = 0; j < strMatrix; j++) {
+				if (in.hasNextDouble()) {
+					matrix[i][j] = in.nextDouble();
+				}
+			}
+		}
+		return matrix;
+	}
+}
