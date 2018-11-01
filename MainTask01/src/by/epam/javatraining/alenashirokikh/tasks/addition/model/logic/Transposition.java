a70bea6e188d4ecf4a6219718df4b5a29c5b83bf
AlenaShi <helen.shir.helen@gmail.com>
2018-11-01 21:52:18 +0300

    addition

--- MainTask01/src/by/epam/javatraining/alenashirokikh/tasks/addition/model/logic/Transposition.java
+++ MainTask01/src/by/epam/javatraining/alenashirokikh/tasks/addition/model/logic/Transposition.java
@@ -4,21 +4,24 @@ public class Transposition {
 	public static double[][] transposeMatrix(double[][] matrix) {
 		int k = 0;
 		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0+k; j < matrix[0].length-k-1; j++) {
-				if(matrix[matrix.length - j - 1][i]== matrix[i][j]){
+			for (int j = 0 + k; j < matrix[0].length - k - 1; j++) {
+				if (matrix.length - j - 1 == i && i == j) {
 					continue;
-				}else{
-				matrix[matrix.length - j - 1][i] = matrix[matrix.length - j - 1][i] + matrix[i][j]
-						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i];
-				matrix[matrix.length - 1 - i][matrix.length - j - 1] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
-						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
-				matrix[j][matrix.length - 1 - i] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
-						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
-				matrix[i][j] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
-						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
-				matrix[matrix.length - j - 1][i] = matrix[matrix.length - j - 1][i] - (matrix[i][j]
-						+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
-			}}
+				} else {
+					matrix[i][j] = matrix[matrix.length - j - 1][i] + matrix[i][j]
+							+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i];
+					matrix[matrix.length - j - 1][i] = matrix[i][j] - (matrix[matrix.length - j - 1][i]
+							+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+					matrix[matrix.length - 1 - i][matrix.length - j - 1] = matrix[i][j]
+							- (matrix[matrix.length - j - 1][i] + matrix[matrix.length - 1 - i][matrix.length - j - 1]
+									+ matrix[j][matrix.length - 1 - i]);
+					matrix[j][matrix.length - 1 - i] = matrix[i][j] - (matrix[matrix.length - j - 1][i]
+							+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+					matrix[i][j] = matrix[i][j] - (matrix[matrix.length - j - 1][i]
+							+ matrix[matrix.length - 1 - i][matrix.length - j - 1] + matrix[j][matrix.length - 1 - i]);
+
+				}
+			}
 			++k;
 		}
 		return matrix;
